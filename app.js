0fec3b680c4c4b72fdb467b70db5b8fdfa812a31
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-09-07 00:29:50 +0530

    cus detailpg changes added

--- app.js
+++ app.js
@@ -12,3 +12,24 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         alert('Invalid username or password');
     }
 });
+
+
+// costomer add
+
+//      document.getElementById('addButton').addEventListener('click', function() {
+//     const itemInput = document.getElementById('itemInput');
+//     const itemName = itemInput.value.trim(); // Get input value and remove extra spaces
+    
+//     if (itemName) { // Check if input is not empty
+//         const itemList = document.getElementById('itemList');
+//         const newItem = document.createElement('li'); // Create a new list item element
+//         newItem.textContent = itemName; // Set the text content to the input value
+//         itemList.appendChild(newItem); // Add the new item to the list
+        
+//         itemInput.value = ''; // Clear the input field
+//     } else {
+//         alert('Please enter an item name!'); // Alert user if input is empty
+//     }
+// });
+
+
